4937aaeddeb6132026a01b6d0606f3b644ff60aa
Student 8 <Student 8@10.8.33.102>
2016-09-16 12:58:10 -0400

    hi

---
+++ Period 8 Java/src/introduction/OOPExample.java
@@ -0,0 +1,10 @@
+package introduction;
+
+public class OOPExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student jillian = new Student("Jillian");
+	}
+
+}
---
+++ Period 8 Java/src/introduction/Student.java
@@ -0,0 +1,13 @@
+package introduction;
+
+public class Student {
+	
+	//fields
+	private String name;
+	
+	//constructor (initialize field)
+	public Student(String name){
+		this.name = name;
+
+	}
+}
