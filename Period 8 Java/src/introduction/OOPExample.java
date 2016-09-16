56675a08abce10c54ab7461d928ccb4a0c859d59
Student 8 <Student 8@10.8.33.102>
2016-09-16 13:06:18 -0400

    hi

--- Period 8 Java/src/introduction/OOPExample.java
+++ Period 8 Java/src/introduction/OOPExample.java
@@ -5,6 +5,11 @@ public class OOPExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Student jillian = new Student("Jillian");
+	jillian.talk();
+	Student joseph = new Student("Joseph");
+	joseph.talk();
+	Student jordan = new Student("Jordan");
+	jordan.talk();
 	}
 
 }
