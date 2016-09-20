ead533f2a465065fd5793411090b49d67b065c4d
Student 8 <Student 8@10.8.33.102>
2016-09-20 13:55:19 -0400

    ...

--- Period 8 Java/src/introduction/RandomDiceRoll.java
+++ Period 8 Java/src/introduction/RandomDiceRoll.java
@@ -3,21 +3,34 @@ package introduction;
 public class RandomDiceRoll {
 
 	public static void main(String[] args) {
+		int[] results = new int [6];
 		// TODO Auto-generated method stub
-		for(int index= 0; index < 10; index++){
-			System.out.println("Roll #" + (index+1) + ": "+rollFairDie());
-			
+		for(int index= 0; index < 10000; index++){
+			int result = rollFairDie();
+			System.out.println("Roll #" + (index+1) + ": "+result);
+			//System.out.println("Roll #" + (index+1) + ": "+rollUnfairDie());
+			results[result-1] ++;
 		}
+
+		for (int i = 0; i<6; i++){
+			System.out.println((i+1) + " appeared " + results[i] + " times appeared.");
+		}
+
 	}
-	
+
 	//return 1,2,3,4,5,6 with equal probability
 	public static int rollFairDie(){
-		
-	double rand =Math.random();//returns a double (0,1)
-	int roll = (int) (6*rand);//[0,5]
-	roll++; //[1,6]
-	return roll;
+
+		double rand =Math.random();//returns a double (0,1)
+		int roll = (int) (6*rand);//[0,5]
+		roll ++; //[1,6]
+		return roll;
 	}
 	public static int rollUnfairDie(){
-
+		int[] conceptualSides = {1,2,3,4,4,4,5,5,6};
+		int conceptualOutcomes = conceptualSides.length; //length of array
+		double rand = Math.random();//returns a double (0,1)
+		int roll = (int) (conceptualOutcomes*rand);
+		return conceptualSides[roll];
+	}
 }
