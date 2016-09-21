dde4c749fcefa2faac0d5e2830371ca0bed06205
Student 8 <Student 8@10.8.33.102>
2016-09-21 14:01:19 -0400

    ...

--- Period 8 Java/src/introduction/StringPractice.java
+++ Period 8 Java/src/introduction/StringPractice.java
@@ -6,14 +6,20 @@ public class StringPractice {
 		String text1 = new String("Hello World");
 		String text2="Hello World";//same as above
 		
-		if (text1.equals (text2)){
+		if (text1.equals (text2)){ //.equals es muy importante
 			System.out.println("these strings are equal:");
 		}
-	prl(text1);
-	prl(text2);
+		prl(text1);
+		prl(text2);
 	
+		String word1 = "Aardvark";
+		String word2 = "Zyzzyva";
+	
+		if(word1.compareTo(word2) <0){
+			prl("word1 comes before word2");
+		}
 	}
-public static void prl(String s){
-	System.out.println(s);
-}
+	public static void prl(String s){
+		System.out.println(s);
+	}
 }
