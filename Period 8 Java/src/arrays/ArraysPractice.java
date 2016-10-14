3a2b1717329772dfb644e6f4ca70aa1c5e6c5fc3
simonblast0 <swk1124@gmail.com>
2016-10-14 13:51:27 -0400

    ...

---
+++ Period 8 Java/src/arrays/ArraysPractice.java
@@ -0,0 +1,45 @@
+package arrays;
+
+public class ArraysPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] zeros1 = {0,0,0};
+		int[] zeros2 = new int[3];
+		//example
+		boolean[] booleans = new boolean[3];
+		//iterate (2 ways)
+		//FIRST METHOD: "FOR LOOP"
+		//   - the index of the data is important to reference
+		//   - you need to customize how you iterate 
+		//(increase by 2, backwards, etc...)
+		for(int index = 0; index< booleans.length; index ++){
+			System.out.println(index+") "+booleans[index]); 
+		}
+		//SECOND METHOD: "FOR-EACH"
+		//always goes in order, does not keep track of index
+		//easier to type
+		//don't need to customize
+		//automatically assignes a "handle"
+		for(boolean b: booleans){
+			System.out.println(b+"");
+		}
+		//	OBJECT ARRAYS
+		String[] someStrings1 = new String[3];
+		String[] someStrings2 = {"a","b","c"};
+		
+		for (String s: someStrings1){
+			System.out.println(s);
+		}
+	}
+}
+//Arrays
+//Fixed length
+//indices start at zero
+//indexed (array)
+//common data type (e.g. "student[]")
+//arrays of any type must be Object[]
+//		
+//		if you want to put different primitive types into an array,
+//you must use their Wrapper class
