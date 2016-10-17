646b294d8562ce9235651cf99d409bca957c7fa0
simonblast0 <swk1124@gmail.com>
2016-10-17 14:04:11 -0400

    ...

---
+++ Period 8 Java/src/arrays/SampleElement.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class SampleElement {
+
+	//field
+		private int number;
+		
+		//constructor
+		public SampleElement(int value){
+			number = value;
+		}
+		
+		public int getNumber(){
+			return number;
+		}
+		
+		
+		//the value of number is stored as a field
+		public void increase(){
+			number++;
+		}
+	
+
+}
