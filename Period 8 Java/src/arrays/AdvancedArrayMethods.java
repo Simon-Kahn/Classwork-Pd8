ae1bb35b1a54e3333d0bc88486280fa7ecf1f719
simonblast0 <swk1124@gmail.com>
2016-10-20 14:02:54 -0400

    ...

--- Period 8 Java/src/arrays/AdvancedArrayMethods.java
+++ Period 8 Java/src/arrays/AdvancedArrayMethods.java
@@ -4,15 +4,26 @@ public class AdvancedArrayMethods {
 static int[] array;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-array = new int[100];//full of zeroes
-int[] someArray= new int[50];
-someArray= array;
-someArray = new int[50];
-System.out.println(someArray.length);
-someArray[30]=1;
-System.out.println(array[30]);
- methodA(someArray);
+//array = new int[100];//full of zeroes
+//int[] someArray= new int[50];
+//someArray= array;
+//someArray = new int[50];
+//System.out.println(someArray.length);
+//someArray[30]=1;
+//System.out.println(array[30]);
+// methodA(someArray);
+String[] array = {"a","b","c","d","e","f","g","h"};
+swap(array,0,array.length-1);
+
 	}
+		
+	private static void swap(String[] arr, int a, int b) {
+		// TODO Auto-generated method stub
+		String placeholder = arr[b];
+		arr[b]= arr[a];
+		arr[a]=placeholder;
+	}
+
 	private static void methodA(int[] someArray) {
 		// TODO Auto-generated method stub
 		int[] newArray = new int [someArray.length];
