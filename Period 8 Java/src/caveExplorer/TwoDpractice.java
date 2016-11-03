994de8654d32e50c6d573de6d51c196856823ebd
simonblast0 <swk1124@gmail.com>
2016-11-03 14:02:01 -0400

    ...

--- Period 8 Java/src/caveExplorer/TwoDpractice.java
+++ Period 8 Java/src/caveExplorer/TwoDpractice.java
@@ -2,17 +2,66 @@ package caveExplorer;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class TwoDpractice {
-
+	public static Scanner in =new Scanner(System.in);
+	static String[][] arr2D;
+	static String[][]pic;
+	static int starti;
+	static int startj;
+	static int treasurei;
+	static int treasurej;
 	public static void main(String[] args) {
+		arr2D = new String[5][5];
+		pic = new String[5][5];
+		for (int row = 0; row <arr2D.length;row++){
+			for (int col = 0;col <arr2D[row].length;col++){
+				arr2D[row][col]="("+row+", " +col+ ")";
+			}
+		}
+			starti=2;
+			startj=2;
+			treasurei=4;
+			treasurej=3;
 
-		boolean[][] mines = new boolean[6][6];
-		plantMines(mines);
+			startExploring();
+		
 
-		String[][] field = createField(mines);
+
+
+
+
+
+		//		boolean[][] mines = new boolean[15][100];
+		//		plantMines(mines);
+		//
+		//		String[][] field = createField(mines);
+		//		printPic(field);
+		//		drawGrid(6,7);
+	}
+	private static void startExploring() {
+		while(true) {
+			System.out.println("You are in room " + arr2D[starti][startj] + ".");
+			System.out.println("What do you want to do?");
+			if(starti==treasurei&&startj==treasurej){
+				System.out.println("YOU WIN");
+				break;
+			}
+			String input = in.nextLine();
+			int[] newCoordinates = interpretInput(input);
+			starti = newCoordinates[0];
+			startj = newCoordinates[1];
+
+
+
+
+		}
+
 
 	}
+
+
 
 	private static String[][] createField(boolean[][] mines) {
 		// TODO Auto-generated method stub
@@ -28,22 +77,87 @@ public class TwoDpractice {
 		}
 		return field;
 	}
-
-	private static String countNearby(boolean[][] mines, int row, int col) {
-		for (int r = row - 1; r < row + 1; r++) {
-			for (int c = col - 1; c <= col + 1; c++) {
-
-				if(r>=0&&r<mines.length&& c>=0 && c<mines[0].length){
-					
-				}
-			}
-	
+	private static int[] interpretInput(String input) {
+		while (!isValid(input)){
+			System.out.println("Sorry! in this game, you can only use WASD");
+			System.out.println("tell me again what you would like to do");
+			input=in.nextLine();
 		}
+		int currenti=starti;
+		int currentj=startj;
+
+		input = input.toLowerCase();
+		if(input.equals("w")){
+			currenti--;
+		}
+		if(input.equals("s")){
+			currenti++;
+		}
+		if(input.equals("a")){
+			currentj--;
+		}
+		if(input.equals("d")){
+			currentj++;
+		}
+		int[] newCoordinates ={starti, startj};
+		if(currenti>=0&&currenti<arr2D.length&&currentj>=0&&currentj<arr2D[0].length){
+			newCoordinates[0]=currenti;
+			newCoordinates[1]=currentj;
+		}
+		else{System.out.println("You feel your face hit a wall that you, for some inexplicable reason, chose to walk into."+"pick another direction");
+		}
+		return newCoordinates;
+	}
+
+
+	private static boolean isValid(String input) {
+		String[] validKeys ={"w","a","s","d"};
+		for(String key :validKeys){
+			if(input.toLowerCase().equals(key)){
+				return true;
+			}
+		}
+		return false;
+	}
+	private static String countNearby(boolean[][] mines, int row, int col) {
+		//		for (int r = row - 1; r < row + 1; r++) {
+		//			for (int c = col - 1; c <= col + 1; c++) {
+		//
+		//				if(r>=0&&r<mines.length&& c>=0 && c<mines[0].length){
+		//					
+		//				}
+		//			}
+		//	
+		//		}
+		//this method only considers actual elements
+		//		int count = 0;
+		//		for(int r = 0; r< mines.length;r++){
+		//			for (int c=0; c<mines[r].length;c++){
+		//				if(Math.abs(r-row)+Math.abs(c-col)==1 && mines[r][c]){
+		//					count++;
+		//				}
+		//			}
+		//		}
+		//	return""+ count;
+		//this method allows you to be most specific
+		//for example you only want north and east
+		int count=0;
+		count+=isValidAndTrue(mines,row-1,col);
+		count+=isValidAndTrue(mines,row+1,col);
+		count+=isValidAndTrue(mines,row,col-1);
+		count+=isValidAndTrue(mines,row,col+1);
+		return "" +count;
+	}
+
+	private static int isValidAndTrue(boolean[][] mines, int i, int j) {
+		if(i>=0 && i <mines.length && j>=0&&j<mines[0].length&& mines[i][j])
+			return 1;
+		else return 0;
 	}
 
 	private static void plantMines(boolean[][] mines) {
 		// TODO Auto-generated method stub
-		int numberOfMines = 10;
+		int numberOfMines = 50;
 		while (numberOfMines > 0) {
 			int row = (int) (Math.random() * mines.length);
 			int col = (int) (Math.random() * mines[0].length);
@@ -152,6 +266,41 @@ public class TwoDpractice {
 			}
 			System.out.print("\n");
 		}
+
+	}
+	public static void drawGrid(int gridRow, int gridCol) {
+		// String[] xox = {"x","o","x","o","x"};
+		// System.out.println(Arrays.toString(xox));
+
+		// String[][] arr2D = new String[5][4];
+
+		// for (int row = 0; row < arr2D.length; row++) {
+		// for (int col = 0; col < arr2D[row].length; col++) {
+		// arr2D[row][col] = "(" + row + ", " + col + ")";
+		// }
+		// }
+
+		//// every element in a 2D array is itself an array
+		// for (String[] row : arr2D) {
+		// System.out.println(Arrays.toString(row));
+		// }
+
+		String[][] pic = new String[((gridRow*3)+1)][((gridCol*3)+1)];
+
+		for (int row = 0; row < gridRow; row++) {
+			for (int col = 0; col < pic[row].length; col++) {
+				pic[row][col] = " ";
+			}
+		}
+
+
+
+		for (String[] row : pic) 
+			for (String col : row) {
+				System.out.print(col);
+			}
+		System.out.print("\n");
+
 
 	}
 
