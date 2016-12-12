8f131785f32f86e3baba4738b01759ed5a0de746
simonblast0 <swk1124@gmail.com>
2016-12-12 13:26:11 -0500

    ...

--- Period 8 Java/src/guiPractice/components/TextLabel.java
+++ Period 8 Java/src/guiPractice/components/TextLabel.java
@@ -36,8 +36,10 @@ public class TextLabel extends Component {
 	public void update(Graphics2D g) {
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.black);
+		if(text != null){
 		g.setFont(new Font(font,Font.PLAIN,size));
 		g.drawString(text, 4, getHeight()-5);
-	}
+		}
+		}
 
 }
