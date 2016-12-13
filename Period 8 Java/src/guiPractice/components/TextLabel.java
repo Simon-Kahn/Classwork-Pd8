d9f4fb9b34403972122f88ba0daca29a523832bb
simonblast0 <swk1124@gmail.com>
2016-12-13 12:55:56 -0500

    Actually Works

--- Period 8 Java/src/guiPractice/components/TextLabel.java
+++ Period 8 Java/src/guiPractice/components/TextLabel.java
@@ -6,40 +6,67 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
 public class TextLabel extends Component {
-//fields
+
+	//FIELDS
 	private String text;
 	private String font;
 	private int size;
-	
-	
-	
-	
-	public TextLabel(int x, int y, int w, int h,String text) {
+
+	public TextLabel(int x, int y, int w, int h, String text) {
 		super(x, y, w, h);
-		this.text=text;
-		font="Helvetica";
-		size=20;
+		this.text = text;
+		font = "Helvetica";
+		size = 20;
 		update();
 	}
 
 	public void setText(String s){
-		this.text=s;
+		this.text = s;
 	}
-	public void setFont(String font){
-		this.font=font;
-	}
+
 	public void setSize(int size){
-		this.size=size;
+		this.size = size;
 	}
-	
+
+	public void setFont(String font){
+		this.font = font;
+	}
+
+
+
+	public String getText() {
+		return text;
+	}
+
+	public String getFont() {
+		return font;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
 	@Override
 	public void update(Graphics2D g) {
-		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.black);
 		if(text != null){
-		g.setFont(new Font(font,Font.PLAIN,size));
-		g.drawString(text, 4, getHeight()-5);
+			g.setFont(new Font(font,Font.PLAIN,size));
+			g.drawString(text, 4, getHeight()-5);
 		}
-		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- Period 8 Java/src/guiPractice/sampleGames/MouseFollower.java
+++ Period 8 Java/src/guiPractice/sampleGames/MouseFollower.java
@@ -3,15 +3,15 @@ package guiPractice.sampleGames;
 import guiPractice.GUIApplication;
 
 public class MouseFollower extends GUIApplication {
+
 	private CoordinateScreen coordScreen;
 	
-	public MouseFollower() {
-
+	public static void main (String[] args){
+		new MouseFollower();
 	}
-
 	@Override
 	protected void initScreen() {
-		coordScreen = new CoordinateScreen(getWidth(),getHeight());
+		coordScreen = new CoordinateScreen(getWidth(), getHeight());
 		setScreen(coordScreen);
 	}
 
