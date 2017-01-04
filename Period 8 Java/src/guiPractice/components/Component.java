746ccd6043cac72850c6cd79d7f11222c2fe885d
simonblast0 <swk1124@gmail.com>
2017-01-04 14:03:49 -0500

    ...

--- Period 8 Java/src/guiPractice/components/Component.java
+++ Period 8 Java/src/guiPractice/components/Component.java
@@ -40,6 +40,14 @@ public abstract class Component implements Visible {
 		return x;
 	}
 
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
 	@Override
 	public int getY() {
 		return y;
--- Period 8 Java/src/guiPractice/sampleGames/MouseFollower.java
+++ Period 8 Java/src/guiPractice/sampleGames/MouseFollower.java
@@ -4,12 +4,16 @@ import guiPractice.GUIApplication;
 
 public class MouseFollower extends GUIApplication {
 
+	public MouseFollower(int width2, int height2) {
+		super(width2, height2);
+		// TODO Auto-generated constructor stub
+	}
 	public static CoordinateScreen coordScreen;
 	public static MovementScreen moveScreen;
 	public static MouseFollower game;
 	
 	public static void main (String[] args){
-		game= new MouseFollower();
+		game= new MouseFollower(800, 500);
 		Thread app = new Thread(game);
 		app.start();
 	
